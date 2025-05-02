9678bc7f71329e0a1be67cf2fb6f1dea0ce7030e
Lenka Koubková <lvseteckova@gmail.com>
2025-05-02 13:48:49 +0200

    vložení index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,17 +3,7 @@ import { ShopItem } from '../components/ShopItem';
 import '../global.css';
 import './index.css';
 
-// TODO nezapomeňte nastavit svůj login – jednoznačný identifikátor (třeba název účtu na GitHubu)
-//const login = ""
-
-const response = await fetch(
-  'https://nakupy.czechitas.dev/api/mon',
-  {
-    headers: {
-      Authorization: login,
-    },
-  },
-);
+const response = await fetch('https://nakupy.czechitas.dev/api/mon')
 const list = await response.json();
 
 const HomePage = () => (
@@ -33,6 +23,7 @@ const HomePage = () => (
         {list.map((item) => (
           <ShopItem 
             key={item.id}
+            id={item.id}
             name={item.product}
             amount={item.amount + ' ' + item.unit}
             bought={item.done}
@@ -45,9 +36,9 @@ const HomePage = () => (
 
 document.querySelector('#root').innerHTML = render(<HomePage />);
 
-document.querySelector('.newitem-form')
-  .addEventListener('submit', async (e) => {
-    e.preventDefault();
+const handleSubmit = async (event) => {
+  event.preventDefault();
+}
 
     const nameInput = document.querySelector('#input-name');
     const amountInput = document.querySelector('#input-amount');
@@ -60,18 +51,59 @@ document.querySelector('.newitem-form')
       done: false,
     };
 
-    await fetch(
-      'https://nakupy.czechitas.dev/api/mon',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: login,
-        },
-        body: JSON.stringify(body),
+    const resp = await fetch ("https://nakupy.czechitas.dev/api/mon", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+      body: JSON.stringify(body),
+    });
 
+   if (!resp.ok) {
+    alert("Něco se nepovedlo, zkuste to za chvíli znovu.");
+   } else {
+    window.location.reload();
+   };
+  
+
+const handleDelete = async (event) => {
+  const id = event.target.dataset.id;
+  const resp = await fetch(`https://nakupy.czechitas.dev/api/mon/${id}`, {
+    method: "DELETE",
+  });
+
+  if(!resp.ok) {
+    alert("Něco se nepovedlo, zkuste to za chvíli znovu.");
+  } else {
     window.location.reload();
   }
-);
+};
+
+const handleTick = async (event) => {
+  const id = event.target.dataset.id;
+  const resp = await fetch(`https://nakupy.czechitas.dev/api/mon/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({done: true}),
+  });
+
+  if (!resp.ok) {
+    alert("Něco se nepovedlo, zkuste to za chvíli znovu.");
+  } else {
+    window.location.reload();
+  }
+};
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
+document
+  .querySelectorAll(".btn-delete")
+  .forEach((element) => element.addEventListener("click", handleDelete));
+document
+  .querySelectorAll(".btn-tick")
+  .forEach((element) => element.addEventListener("click", handleTick));
+
+    
+
+
